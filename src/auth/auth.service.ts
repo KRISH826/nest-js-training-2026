6fce3db950f07e35019358f47fdc29ec3a50e19c
KRISH826 <krishnendupanja98@gmail.com>
2026-07-25 02:00:59 +0530

    feat: implement login functionality with DTO validation, enhance user service for login, and add JWT authentication guard

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
-import { RegisterDto } from './auth.dto';
+import { loginDto, RegisterDto } from './auth.dto';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     constructor(private readonly userService: UserService, private jwtService: JwtService) { }
-    getLogin() {
-        const result = this.userService.loginUser();
-        return result;
+     async getLogin(loginDto: loginDto) {
+        const user = await this.userService.loginUser(loginDto);
+        const payload = { email: user.email, role: user.role };
+        const token = await this.jwtService.signAsync(payload);
+        return {
+            access_token: token,
+            user
+        };
     }
 
     async getRegister(registerDto: RegisterDto) {
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { RegisterDto } from 'src/auth/auth.dto';
+import { loginDto, RegisterDto } from 'src/auth/auth.dto';
 import { User } from './user.schema';
 
 @Injectable()
@@ -32,9 +32,15 @@ export class UserService {
         }
     }
 
-    loginUser() {
-        return {
-            message: 'User Logged In Successfully',
+    async loginUser(loginDto: loginDto) {
+        try {
+            const user = await this.userModel.findOne({ email: loginDto.email });
+            if(!user) throw new Error('User Not Found');
+            if(user.password !== loginDto.password) throw new Error('Invalid Password');
+            return user;
+        } catch (err) {
+            const e = err as {code?: number, message?: string}
+            throw new Error(e.message);
         }
     }
 }
