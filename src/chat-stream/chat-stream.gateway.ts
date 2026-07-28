c0b2d9b4cbd22a533660f3b112cd331c7fef0b84
KRISH826 <krishnendupanja98@gmail.com>
2026-07-28 10:57:33 +0530

    feat: implement ChatStreamGateway with room management and message handling

--- src/chat-stream/chat-stream.gateway.ts
+++ src/chat-stream/chat-stream.gateway.ts
@@ -1,14 +1,52 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
   cors: {
-    origin: '*',
+    origin: 'http://localhost:3000',
   },
 })
-export class ChatStreamGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    console.log("hello new message")
-    return 'Hello world!';
+export class ChatStreamGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server!: Server;
+  private readonly ROOM = 'Coders Lounge';
+
+  @SubscribeMessage('joinRoom')
+  async handleMessage(@ConnectedSocket() client: Socket, @MessageBody() payload: string) {
+    console.log(`${payload} is joining the room`);
+    await client.join(this.ROOM);
+    this.server.to(this.ROOM).emit('roomNotice', {
+      username: payload,
+      message: `${payload} has joined the room`,
+    });
+    console.log('Recived', payload);
+  }
+
+  @SubscribeMessage('messageList')
+  async handleMessageList(@ConnectedSocket() client: Socket, @MessageBody() payload: string) {
+    this.server.to(this.ROOM).emit('messageList', payload);
+    console.log('Recived', payload);
+  }
+
+  @SubscribeMessage('chatMessage')
+  async handleChatMessage(@ConnectedSocket() client: Socket, @MessageBody() payload: string) {
+    this.server.to(this.ROOM).emit('chatMessage', {
+      username: client.id,
+      message: payload
+    });
+    console.log(client.id ,":", payload);
+  }
+
+  @SubscribeMessage('leaveRoom')
+  async handleLeaveRoom(@ConnectedSocket() client: Socket, @MessageBody() payload: string) {
+    console.log(`${payload} is leaving the room`);
+    await client.leave(this.ROOM);
+  }
+
+  handleConnection(connect: any, ...args: any[]) {
+    console.log(`hello new connection ${connect.id}`)
+  }
+  handleDisconnect(connect: any) {
+    console.log(`hello new disconnection ${connect.id}`)
   }
 }
