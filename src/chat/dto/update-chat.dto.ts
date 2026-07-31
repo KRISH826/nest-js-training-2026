e61f3b346ccf47dc4c106ad16c59b5bf3ed6f124
KRISH826 <krishnendupanja98@gmail.com>
2026-08-01 00:46:58 +0530

    feat: define CreateChatDto and UpdateChatDto with validation rules; implement Chat entity with Mongoose schema

--- src/chat/dto/update-chat.dto.ts
+++ src/chat/dto/update-chat.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateChatDto } from './create-chat.dto';
+import { IsEmpty, IsString, MaxLength } from 'class-validator';
 
-export class UpdateChatDto extends PartialType(CreateChatDto) {}
+export class UpdateChatDto extends PartialType(CreateChatDto) {
+    @IsString()
+    @IsEmpty({ message: 'message cannot be empty' })
+    @MaxLength(1000)
+    message!: string
+}
