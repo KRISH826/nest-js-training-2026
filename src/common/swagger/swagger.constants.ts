0e797ef076c0498c410b260276307abd36336956
KRISH826 <krishnendupanja98@gmail.com>
2026-08-09 01:51:10 +0530

    feat: implement Swagger integration for chat functionalities; add API decorators for chat room and chat message operations

---
+++ src/common/swagger/swagger.constants.ts
@@ -0,0 +1,9 @@
+export const SWAGGER_TAGS = {
+  AUTH: 'Auth',
+  USER: 'User Profile',
+  CHAT_ROOM: 'Chat Rooms',
+  CHAT: 'Chat History',
+  COURSE: 'Courses',
+} as const;
+
+export const SWAGGER_AUTH_NAME = 'Bearer';
